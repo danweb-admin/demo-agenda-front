ca0da22158e596aed6b73228a3c8616de968bc20
William Nascimento <william.nascimento2010@gmail.com>
2025-08-20 18:42:23 -0300

    teste ip externo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,19 @@ import { SpinnerService } from './shared/services/spinner.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent  implements OnInit{
+  ngOnInit(): void {
+    this.getExternalIP();
+  }
+  
+  
+  async getExternalIP(): Promise<void> {
+    try {
+      const response = await fetch('https://api.ipify.org?format=json');
+      const data = await response.json();
+      console.log('IP externo:', data.ip);
+    } catch (error) {
+      console.error('Erro ao obter IP:', error);
+    }
+  }
 }
