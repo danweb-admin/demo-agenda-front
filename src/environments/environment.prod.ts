3a4f78c6770e0ae5d0e2d7b683bcd1f3c41fca68
William Nascimento <william.nascimento2010@gmail.com>
2025-10-25 09:23:16 -0300

    ajuste url api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
   // URL_API: 'http://localhost:80÷80',
-  URL_API: 'http://54.82.193.15:5000',
-  //URL_API: 'https://backend.swr-locacoes-agenda.online', 
+  // URL_API: 'http://54.82.193.15:5000',
+  URL_API: 'https://backend.sjc-laser-agenda.site', 
   hmr: false,  
   role: ''
 };
