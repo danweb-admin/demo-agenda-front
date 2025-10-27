9fb9d36e3abb77e2ee314a57e63a56ee159da9f7
William Nascimento <william.nascimento2010@gmail.com>
2025-10-26 21:59:09 -0300

    adicionado cor

--- src/app/pages/equipaments/components/equipaments-dialog/equipaments-dialog.component.ts
+++ src/app/pages/equipaments/components/equipaments-dialog/equipaments-dialog.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Inject, OnInit } from '@angular/core';
 import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { ToastrService } from 'ngx-toastr';
@@ -11,69 +11,92 @@ import { EquipamentsService } from 'src/app/shared/services/equipaments.service'
 import { SpecificationsService } from 'src/app/shared/services/specifications.service';
 
 @Component({
-		selector: 'app-equipaments-dialog',
-		templateUrl: 'equipaments-dialog.component.html',
-		styleUrls: ['./equipaments-dialog.component.scss']
-	})
-	export class EquipamentsDialogComponent implements OnInit {
-		form: FormGroup;
-		isAddMode: boolean;
-		id: string;
-		list: EquipamentSpecifications[] = [];
-		consumables: Consumable[] = [];
-		consumableArray: [] = [];
+	selector: 'app-equipaments-dialog',
+	templateUrl: 'equipaments-dialog.component.html',
+	styleUrls: ['./equipaments-dialog.component.scss']
+})
+export class EquipamentsDialogComponent implements OnInit, AfterViewInit {
+	form: FormGroup;
+	isAddMode: boolean;
+	id: string;
+	list: EquipamentSpecifications[] = [];
+	consumables: Consumable[] = [];
+	consumableArray: [] = [];
+	cores = [
+		{ nome: 'Vermelho', hex: '#FF4C4C' },
+		{ nome: 'Verde', hex: '#4CAF50' },
+		{ nome: 'Azul', hex: '#2196F3' },
+		{ nome: 'Amarelo', hex: '#FFEB3B' },
+		{ nome: 'Laranja', hex: '#FF9800' },
+		{ nome: 'Roxo', hex: '#9C27B0' },
+		{ nome: 'Ciano', hex: '#00BCD4' },
+		{ nome: 'Rosa', hex: '#E91E63' },
+		{ nome: 'Lima', hex: '#CDDC39' },
+		{ nome: 'Turquesa', hex: '#40E0D0' },
+		{ nome: 'Azul Claro', hex: '#03A9F4' },
+		{ nome: 'Verde Água', hex: '#66BB6A' },
+		{ nome: 'Coral', hex: '#FF7F50' },
+		{ nome: 'Dourado', hex: '#FFD700' },
+		{ nome: 'Lavanda', hex: '#B39DDB' }
+	];
+	
+	constructor(
+		public dialogRef: MatDialogRef<EquipamentsDialogComponent>,
+		@Inject(MAT_DIALOG_DATA) public data: any,
+		private specificationService: SpecificationsService,
+		private equipamentsService: EquipamentsService,
+		private consumablesService: ConsumablesService,
+		private formBuilder: FormBuilder,
+		private toastr: ToastrService) {}
 
-		constructor(
-			public dialogRef: MatDialogRef<EquipamentsDialogComponent>,
-			@Inject(MAT_DIALOG_DATA) public data: any,
-			private specificationService: SpecificationsService,
-			private equipamentsService: EquipamentsService,
-			private consumablesService: ConsumablesService,
-			private formBuilder: FormBuilder,
-			private toastr: ToastrService) {}
-
+		ngAfterViewInit(): void {
+		setTimeout(() => {
+			this.ajustesCSS();
+		},500);
+		}
+		
 		async loadSpecifications(): Promise<void> {
 			await this.specificationService.loadSpecifications().toPromise().then((data) => {
 				localStorage.setItem('specificationsList',JSON.stringify(data));
 			}); 
 		}
-
+		
 		loadConsumables(): void {
 			this.consumablesService.loadConsumables().subscribe((resp: Consumable[]) => {
 				this.consumables = resp;
 				this.createConsumableForms();
 			}); 
 		}
-
+		
 		createConsumableForms(): void {
 			this.consumables.forEach(item => {
 				const existingItem = this.data.element?.equipamentConsumables.find(e => e.consumableId === item.id);
-
+				
 				const formGroup = this.formBuilder.group({
 					name: [{value: item.name, disabled: true}],
 					active: [existingItem ? existingItem.active : item.active, Validators.required],
 					value: [existingItem ? existingItem.value.toFixed(2).replace('.',',') : 0, Validators.required],
 					equipamentId: [existingItem ? this.data.element.id : null],
 					consumableId: [item.id],
+					color: [''] ,
 					createdAt: existingItem ? existingItem.createdAt : new Date()
 				});
-		
+				
 				if (existingItem) {
 					formGroup.addControl('id', new FormControl(existingItem.id));
 				}
-		
+				
 				this.equipamentConsumables.push(formGroup);
 			});
 		}
-
+		
 		ngOnInit(): void {
 			this.createForm();
 			this.loadSpecifications();
 			this.loadConsumables();
-			console.log(this.consumables);
 			
 		}
-
+		
 		public createForm(){
 			let list = JSON.parse(localStorage.getItem('specificationsList'));
 			let arr = [];
@@ -88,7 +111,7 @@ import { SpecificationsService } from 'src/app/shared/services/specifications.se
 				} as EquipamentSpecifications;
 				arr.push(this.buildEquipamentSpecifications(equipamentSpecification));
 			});
-
+			
 			this.form = this.formBuilder.group({
 				id:  [this.data.element?.id || ''],
 				name: [this.data.element?.name || '', Validators.required],
@@ -96,15 +119,16 @@ import { SpecificationsService } from 'src/app/shared/services/specifications.se
 				order: [this.data.element?.order || 0],
 				createdAt: [this.data.element?.createdAt || new Date()],
 				updatedAt: [this.data.element?.updatedsAt || null],
+				color: [this.data.element?.color || null],
 				equipamentSpecifications: this.formBuilder.array(arr),
 				equipamentConsumables: this.formBuilder.array(this.consumableArray)
 			});
 		}
-
+		
 		get equipamentConsumables(): FormArray {
 			return this.form.get('equipamentConsumables') as FormArray;
 		}
-
+		
 		returnTrueOrFalse(value){
 			let retorno = false;
 			this.data.element?.equipamentSpecifications.forEach(item => {
@@ -116,7 +140,7 @@ import { SpecificationsService } from 'src/app/shared/services/specifications.se
 			
 			return retorno;
 		}
-
+		
 		buildEquipamentSpecifications(equipamentSpecification: EquipamentSpecifications){
 			return this.formBuilder.group({
 				specificationId: equipamentSpecification.specificationId,
@@ -124,11 +148,11 @@ import { SpecificationsService } from 'src/app/shared/services/specifications.se
 				name: equipamentSpecification.name
 			});
 		}
-
+		
 		onNoClick(): void {
 			this.dialogRef.close();
 		}
-
+		
 		onSubmit(){
 			
 			this.adjustFormValues();
@@ -144,12 +168,20 @@ import { SpecificationsService } from 'src/app/shared/services/specifications.se
 				});
 			}
 		}
-
+		
 		adjustFormValues(){
 			this.equipamentConsumables.controls.forEach((control, index) => {
 				const currentValue = control.get('value').value.toString();
 				const newValue = currentValue.replace(',', '.');
 				control.get('value').patchValue(newValue);
-			  });
+			});
+		}
+		
+		ajustesCSS(){
+			var mat_select = document.getElementsByClassName('mat-select');
+			
+			for (var i = 0; i < mat_select.length; i++) {
+				mat_select[i].setAttribute('style', 'display: contents');
+			}
 		}
 	}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   URL_API: 'http://localhost:8080',
-    // URL_API: 'http://44.206.110.31:5000',
+  // URL_API: 'https://backend.sjc-laser-agenda.site', 
 
   hmr: false,  
   role: ''
