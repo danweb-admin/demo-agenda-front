977382b9d0ba0bbc768a057528c90346474016f7
William Nascimento <william.nascimento2010@gmail.com>
2026-01-27 16:24:49 -0300

    url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,10 @@
 export const environment = {
   production: false,
   // URL_API: 'http://localhost:80÷80',
-  URL_API: 'http://95.111.226.231:32060',
+  // URL_API: 'http://95.111.226.231:32060',
   // URL_API: 'https://backend.sjc-laser-agenda.site',
+    URL_API: 'https://backend.belacrio-agenda.online',
+
   hmr: false,  
   role: ''
 };
